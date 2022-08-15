6cf0e76f248927ce8d636ac17e77a943e979aef4
AayushMohan <aayushmohan1702@gmail.com>
2022-08-16 03:43:38 +0530

    Added: Product Feed Component

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import Head from "next/head";
 import Image from "next/image";
 import Banner from "../components/Banner";
 import Header from "../components/Header";
+import ProductFeed from "../components/ProductFeed";
 
 const Home: NextPage = () => {
   return (
@@ -19,6 +20,7 @@ const Home: NextPage = () => {
         <Banner />
 
         {/* ProductFeed */}
+        <ProductFeed />
       </main>
     </div>
   );
