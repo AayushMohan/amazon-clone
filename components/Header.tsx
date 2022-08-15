3c5eaab8078bb8737024413bc28cf0934fdfa9c4
AayushMohan <aayushmohan1702@gmail.com>
2022-08-16 00:46:11 +0530

    Added: Header

---
+++ components/Header.tsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      {/* Top nav */}
+      <div></div>
+
+      {/* Bottom Nav */}
+      <div></div>
+    </div>
+  );
+};
+
+export default Header;
