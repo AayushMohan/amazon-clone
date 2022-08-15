6f51690c40a7db19ff4a359535e6f7a39b970f35
AayushMohan <aayushmohan1702@gmail.com>
2022-08-16 01:01:20 +0530

    Added: Amazon Logo

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,13 +3,23 @@ import React from "react";
 
 const Header = () => {
   return (
-    <div>
+    <header>
       {/* Top nav */}
-      <div></div>
+      <div className="flex items-center bg-amazon_blue p-1 flex-grow py-2">
+        <div className="">
+          <Image
+            src="https://links.papareact.com/f90"
+            width={150}
+            height={40}
+            objectFit="contain"
+            className="cursor-pointer"
+          />
+        </div>
 
-      {/* Bottom Nav */}
-      <div></div>
-    </div>
+        {/* Bottom Nav */}
+        <div></div>
+      </div>
+    </header>
   );
 };
 
